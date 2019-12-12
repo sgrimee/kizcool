dc4e11696d8d7f52ab7cd392d62e9dc3d5a84845
sgrimee <sgrimee@cisco.com>
2019-12-12 15:53:36 +0100

    Testing for config package

--- config/config.go
+++ config/config.go
@@ -2,8 +2,14 @@
 package config
 
 import (
+	"fmt"
+	"reflect"
+
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/mitchellh/mapstructure"
+	"github.com/sgrimee/kizcool/group"
 	"github.com/spf13/viper"
+	"github.com/vapourismo/knx-go/knx/cemi"
 )
 
 // Defaults for config file
@@ -15,69 +21,104 @@ const (
 
 var usingConfigFile bool
 
+var v *viper.Viper
+
+func init() {
+	v = viper.New()
+	v.SetDefault("base_url", TahomaBaseURL)
+
+	v.SetEnvPrefix("KIZ")
+	v.AutomaticEnv()
+}
+
 // Username returns the username
 func Username() string {
-	return viper.GetString("username")
+	return v.GetString("username")
 }
 
 // SetUsername sets the username
 func SetUsername(username string) {
-	viper.Set("username", username)
+	v.Set("username", username)
 }
 
 // Password returns the password
 func Password() string {
-	return viper.GetString("password")
+	return v.GetString("password")
 }
 
 // SetPassword sets the password
 func SetPassword(password string) {
-	viper.Set("password", password)
+	v.Set("password", password)
 }
 
 // BaseURL returns the BaseURL
 func BaseURL() string {
-	return viper.GetString("base_url")
+	return v.GetString("base_url")
 }
 
 // SetBaseURL sets the BaseURL
 func SetBaseURL(url string) {
-	viper.Set("base_url", url)
+	v.Set("base_url", url)
 }
 
 // SessionID returns the SessionID
 func SessionID() string {
-	return viper.GetString("session_id")
+	return v.GetString("session_id")
 }
 
 // SetSessionID stores the session ID in the configuration file
 func SetSessionID(ID string) {
-	viper.Set("session_id", ID)
+	v.Set("session_id", ID)
+}
+
+// toGaddrHookFunc decodes strings into cemi.GroupAddr integers
+func toGaddrHookFunc() mapstructure.DecodeHookFunc {
+	return func(
+		f reflect.Type,
+		t reflect.Type,
+		data interface{}) (interface{}, error) {
+		// Only handle conversion from string to Uint16 (cemi.GroupAddr)
+		if t.Kind() != reflect.Uint16 {
+			return data, nil
+		}
+		if f.Kind() != reflect.String {
+			return data, nil
+		}
+		str := fmt.Sprintf("%v", data)
+		return cemi.NewGroupAddrString(str)
+	}
+}
+
+// Devices returns the list of group devices defined in the config
+func Devices() ([]group.Device, error) {
+	var result []group.Device
+	opt := viper.DecodeHook(mapstructure.ComposeDecodeHookFunc(
+		toGaddrHookFunc(),
+	))
+
+	err := v.UnmarshalKey("devices", &result, opt)
+	return result, err
 }
 
 // Read reads in config file. It should be called before using other functions in this package.
 // The local directory is searched first, then the user's home directory
 // If no file is found and create is true, a config file with defaults is created.
 func Read(create bool) error {
-	viper.SetConfigName(DefaultConfigFileBaseName) // name of config file (without extension)
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("$HOME")
-	viper.SetDefault("base_url", TahomaBaseURL)
+	v.SetConfigName(DefaultConfigFileBaseName) // name of config file (without extension)
+	v.AddConfigPath(".")
+	v.AddConfigPath("$HOME")
 
-	viper.SetEnvPrefix("KIZ")
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
+	if err := v.ReadInConfig(); err != nil {
 		// create empty config file if needed
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok && create {
 			configFile, _ := homedir.Expand("~/" + DefaultConfigFileBaseName + DefaultConfigFileExtension)
-			viper.SetConfigPermissions(0600)
-			if err := viper.SafeWriteConfigAs(configFile); err != nil {
+			v.SetConfigPermissions(0600)
+			if err := v.SafeWriteConfigAs(configFile); err != nil {
 				usingConfigFile = false
 				return err
 			}
 			usingConfigFile = true
-			return viper.ReadInConfig()
+			return v.ReadInConfig()
 		}
 		usingConfigFile = false
 		return nil
@@ -86,22 +127,22 @@ func Read(create bool) error {
 	return nil
 }
 
+// Write saves the current config to file
+func Write() error {
+	return v.WriteConfig()
+}
+
 // UsingConfigFile is true if a config file is used, false otherwise (e.g. only env variables)
 func UsingConfigFile() bool {
 	return usingConfigFile
 }
 
-// Write saves the current config to file
-func Write() error {
-	return viper.WriteConfig()
-}
-
 // File returns the name and path of the configuration file in use.
 func File() string {
-	return viper.ConfigFileUsed()
+	return v.ConfigFileUsed()
 }
 
 // Debug returns true if debugging was requested
 func Debug() bool {
-	return viper.GetBool("debug")
+	return v.GetBool("debug")
 }
---
+++ config/config_test.go
@@ -0,0 +1,104 @@
+package config
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/vapourismo/knx-go/knx/cemi"
+)
+
+var yamlExample = []byte(`
+debug: true
+password: mysecretpass
+username: me@nowhere.com
+bla: boum
+devices:
+  - label: Spots lit parents
+    deviceurl: io://1111-0000-4444/11111111
+    commands:
+      - commandname: setIntensity
+        groupaddr: 10/0/3
+      - commandname: setOnOff
+        groupaddr: 10/0/4
+    states:
+      - qualifiedname: core:OnOffState
+        groupaddr: 10/0/5
+      - qualifiedname: core:LightIntensityState
+        groupaddr: 10/0/6
+`)
+
+func TestMain(m *testing.M) {
+	v.SetConfigType("yaml")
+	v.ReadConfig(bytes.NewBuffer(yamlExample))
+
+	os.Exit(m.Run())
+}
+
+func TestUsername(t *testing.T) {
+	assert.Equal(t, "me@nowhere.com", Username())
+}
+
+func TestSetUsername(t *testing.T) {
+	SetUsername("notme@somewhere.com")
+	assert.Equal(t, "notme@somewhere.com", Username())
+}
+
+func TestPassword(t *testing.T) {
+	assert.Equal(t, "mysecretpass", Password())
+}
+
+func TestSetPassword(t *testing.T) {
+	SetPassword("newpassword")
+	assert.Equal(t, "newpassword", Password())
+}
+
+func TestBaseURL(t *testing.T) {
+	assert.Equal(t, TahomaBaseURL, BaseURL())
+}
+
+func TestSetBaseURL(t *testing.T) {
+	SetBaseURL("https://happy.com")
+	assert.Equal(t, "https://happy.com", BaseURL())
+}
+
+func TestSessionID(t *testing.T) {
+	assert.Equal(t, "", SessionID())
+}
+
+func TestSetSessionID(t *testing.T) {
+	SetSessionID("1234567890")
+	assert.Equal(t, "1234567890", SessionID())
+}
+
+func TestDevices(t *testing.T) {
+	devices, err := Devices()
+	require.NoError(t, err)
+	require.Equal(t, 1, len(devices))
+	assert.Equal(t, "setOnOff", devices[0].Commands[1].Name)
+	gaddr, _ := cemi.NewGroupAddrString("10/0/4")
+	assert.Equal(t, gaddr, devices[0].Commands[1].GroupAddr)
+}
+
+// func TestRead(t *testing.T) {
+// }
+
+// func TestWrite(t *testing.T) {
+// }
+
+func TestUsingConfigFile(t *testing.T) {
+	usingConfigFile = true
+	assert.True(t, UsingConfigFile())
+	usingConfigFile = false
+	assert.False(t, UsingConfigFile())
+}
+
+func TestFile(t *testing.T) {
+	assert.Equal(t, "", File())
+}
+
+func TestDebug(t *testing.T) {
+	assert.True(t, Debug())
+}
